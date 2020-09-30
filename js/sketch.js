e63fb7cbcf0e1d86cf06ee153dbef10906f92a85
Helen Gilissen <55249087+helengilissen@users.noreply.github.com>
2020-09-30 11:00:53 +0200

    added 6f setting boundaries

---
+++ js/sketch.js
@@ -0,0 +1,134 @@
+function setup() {
+  createCanvas(400,400);
+  
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  background(240);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+
+  
+
+if (mouseX > 375) {
+  background (0);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+  else if (mouseX > 350) {
+    background (21);
+  
+    stroke (255,0,0);
+    strokeWeight (2);
+    line (200,0,200,400);
+}
+
+else if (mouseX > 325) {
+  background (42);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 300) {
+  background (63);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 275) {
+  background (84);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 250) {
+  background (105);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 225) {
+  background (126);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 200) {
+  background (147);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 150) {
+  background (168);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 125) {
+  background (189);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 100) {
+  background (210);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 75) {
+  background (231);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+else if (mouseX > 50) {
+  background (252);
+
+  stroke (255,0,0);
+  strokeWeight (2);
+  line (200,0,200,400);
+}
+
+  else if (mouseX > 0) {
+    background (270);
+  
+    stroke (255,0,0);
+    strokeWeight (2);
+    line (200,0,200,400);
+
+
+}
+
+
+
+
+}
